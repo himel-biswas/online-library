397dca5099511607b9071ecf0947d201c1bd92e0
Himel Biswas <eg.himel@gmail.com>
2017-02-18 20:17:19 +0600

    deleted some unnecessary files

--- bookshop/admin/includes/footer.inc.php
+++
@@ -1,3 +0,0 @@
-<div id="footer-wrap">
-	<p id="legal">Online Book Store (c)2016 Design by <a href="developer.php">Biswas Himel, Ahmed Md Sabbir</a>.</p>
-	</div>
--- bookshop/admin/includes/logo.inc.php
+++
@@ -1,2 +0,0 @@
-<h1>Admin Panel</h1>
-	<h2> Design by Himel</h2>
